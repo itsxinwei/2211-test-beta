cf24a3dcd923add379b689409be3b5946803e457
itsxinwei <itsxinwei@gmail.com>
2021-01-23 10:48:53 +0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+import math
+import sys
+from os import rename
+
+import requests
+
+print (sys.version)
+# print(sys.executable)
+
+
+
+def curseu(target):
+    comma = "fuck u, {}".format(target)
+    return comma
+
+
+print(curseu("peter"))
+
+# r = requests.get("https://vendiscover.com")
+# print(r.status_code)
